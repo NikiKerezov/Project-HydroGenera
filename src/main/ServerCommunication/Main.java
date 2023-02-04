46463b79c7779e220beebef15f9b7c0cc7715064
Vasil Kostomberov <kostomberov@gmail.com>
2023-02-04 17:11:30 +0200

    SOLID principals changes

--- src/main/ServerCommunication/Main.java
+++ src/main/ServerCommunication/Main.java
@@ -1,9 +1,12 @@
 package ServerCommunication;
 
+import LocalData.Models.DataPackage;
+
 import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws URISyntaxException {
-        System.out.println("a");
+        WebSocketConnection wsc = new WebSocketConnection();
+        wsc.sendPackage(new DataPackage(4,"a",4,4, 5, 5, 5));
     }
 }
--- src/main/ServerCommunication/WebSocketConnection.java
+++ src/main/ServerCommunication/WebSocketConnection.java
@@ -13,12 +13,19 @@ public class WebSocketConnection extends Observer implements IServerConnection {
 
     public WebSocketConnection() throws URISyntaxException {
         socket = IO.socket("ws://socket.statistics.green-innovation.bg");
+        socket.on(Socket.EVENT_CONNECT, objects -> {
+            System.out.println(objects);
+        });
+        socket.on(Socket.EVENT_DISCONNECT, objects -> {
+            System.out.println(objects);
+        });
         socket.open();
     }
 
     @Override
     public void sendPackage(DataPackage dataPackage) {
-        socket.emit("data", dataPackage);
+        socket.emit("test", "as");
+        //socket.close();
     }
 
     @Override

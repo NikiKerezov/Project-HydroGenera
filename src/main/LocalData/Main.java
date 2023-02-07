960e0717da3a4d9bb163af19e16e1c5987a8f3f5
NikiKerezov <nikolaaleksandrov123@gmail.com>
2023-02-07 16:25:05 +0200

    Added try/catch to main

--- src/main/LocalData/Main.java
+++ src/main/LocalData/Main.java
@@ -7,7 +7,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Main {
     public static void main(String[] args) {
         DataPackage data = new DataPackage(10, "nz", 1, 2, 3, 4, 5);
-        SaveToFile file = SaveToFile.getInstance();
+        SaveToFile file = null;
+        try {
+            file = SaveToFile.getInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         file.SaveToCsv(data);
 
         // create object mapper

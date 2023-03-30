6cc7e629370b93f2310825e01d822a5c738c4d1d
Vasil Kostomberov <kostomberov@gmail.com>
2023-03-30 18:53:50 +0300

    v

--- src/main/DependancyContainer/Services/JsonReader.java
+++ src/main/DependancyContainer/Services/JsonReader.java
@@ -1,15 +1,23 @@
 package DependancyContainer.Services;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import DependancyContainer.Contracts.IJsonReader;
 import DependancyContainer.Models.Setting;
+import Logger.Contracts.ILogger;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONObject;
 
 
 public class JsonReader implements IJsonReader {
     private ObjectMapper mapper;
+    private ILogger logger;
 
     public JsonReader() {
         mapper = new ObjectMapper();
--- src/main/OnSiteCommunication/Services/OnSiteCommunication.java
+++ src/main/OnSiteCommunication/Services/OnSiteCommunication.java
@@ -1,9 +1,12 @@
 package OnSiteCommunication.Services;
 
+import DependancyContainer.Contracts.IJsonReader;
 import EventEmitter.Observer;
 import LocalData.Models.DataPackage;
 import Logger.Contracts.ILogger;
 import OnSiteCommunication.Contracts.IOnSiteCommunication;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
@@ -38,8 +41,8 @@ public class OnSiteCommunication extends Observer implements IOnSiteCommunicatio
         connect();
     }
 
-    @Override
-    public void receiveJson(JSONObject data) {
+
+ /*   public void receiveJson(JSONObject data) {
         try {
             String filePath = "data.json";
             Path path = Paths.get(filePath);
@@ -56,7 +59,7 @@ public class OnSiteCommunication extends Observer implements IOnSiteCommunicatio
         } catch (IOException e) {
             System.err.println("Error while saving setting to file: " + e.getMessage());
         }
-    }
+    }*/
 
     public void connect(){
         socket.open();
@@ -84,11 +87,53 @@ public class OnSiteCommunication extends Observer implements IOnSiteCommunicatio
         }
     }
 
+    public void receiveJson(String jsonString) throws IOException {
+        /*try {
+            String filePath = "data.json";
+            Path path = Paths.get(filePath);
+            if (!Files.exists(path)) {
+                File file = new File(filePath);
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+            FileWriter fileWriter = new FileWriter(filePath, false);
+            fileWriter.write(data.toString());
+            fileWriter.flush();
+            fileWriter.close();
+            System.out.println("New setting received and saved to file: " + filePath);
+        } catch (IOException e) {
+            System.err.println("Error while saving setting to file: " + e.getMessage());
+        }*/
+        try {
+            String filePath = "data.json";
+            Path path = Paths.get(filePath);
+            if (!Files.exists(path)) {
+                File file = new File(filePath);
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(jsonString);
+            objectMapper.writeValue(new File(filePath), jsonNode);
+            logger.log("New setting received and saved to file: " + filePath, 2);
+        } catch (IOException e) {
+            logger.log("Error while saving setting to file: " + e.getMessage(), 2);
+        }
+    }
+
     public void startListening() {
 
+        /*socket.on("new_setting", (data) -> {
+            try {
+                logger.log("New setting received: " + data[0].toString(), 1);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });*/
         socket.on("new_setting", (data) -> {
             try {
                 logger.log("New setting received: " + data[0].toString(), 1);
+                receiveJson(data[0].toString());
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
